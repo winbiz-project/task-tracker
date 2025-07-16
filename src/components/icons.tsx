c37d1112c8bc6830e329ac606a131a68eccf4503
Ramdani Surya Manggala <ramdanisurya.work@gmail.com>
2025-07-16 07:50:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <logo className="h

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,8 +14,9 @@ export const Icons = {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
-      <circle cx="12" cy="12" r="3" />
+      <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+      <path d="M3 3v5h5" />
+      <path d="M12 7v5l4 2" />
     </svg>
   ),
 };
